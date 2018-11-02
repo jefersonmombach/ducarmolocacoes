32fc3978fb210e28ecbed3435caf595087b5c565
Jeferson Mombach de Sous <jefersonmombach@gmail.com>
2018-11-02 17:17:23 -0300

    feat(ducarmolocacoes): customizando locação, inclusão de produtos.

--- src/main/webapp/app/vendor.ts
+++ src/main/webapp/app/vendor.ts
@@ -43,7 +43,8 @@ import {
     faAsterisk,
     faTasks,
     faHome,
-    faPrint
+    faPrint,
+    faTshirt
 } from '@fortawesome/free-solid-svg-icons';
 
 // Adds the SVG icon to the library so you can use it in your page
@@ -85,5 +86,6 @@ library.add(faSearch);
 library.add(faTrashAlt);
 library.add(faAsterisk);
 library.add(faPrint);
+library.add(faTshirt);
 
 // jhipster-needle-add-element-to-vendor - JHipster will add new menu items here
